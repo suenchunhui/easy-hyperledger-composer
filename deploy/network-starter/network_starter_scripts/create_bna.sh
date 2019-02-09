0d0a15a5e860257f6217c337797cf642af92afe3
duemaster <zhangbsn@gmail.com>
2019-02-09 23:24:12 +0800

    remove debugging echo

--- deploy/network-starter/network_starter_scripts/create_bna.sh
+++ deploy/network-starter/network_starter_scripts/create_bna.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 cd ..
 
-echo "Building BNA ${BNA_FILE_NAME}"
-
 composer archive create --sourceType dir --sourceName /sample-src/composer-bna/ -a /hyperledger/composer/output/${BNA_FILE_NAME}
